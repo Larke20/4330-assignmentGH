f8a46b9819485889cf491ab1a47a8d4cb55f283c
Landon Clarke <89565789+Larke20@users.noreply.github.com>
2024-09-10 01:15:06 -0500

    added program1

---
+++ program1.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int avg(int a, int b){
+	int result;
+	result = (a+b)/2;
+
+	return result;
+}
+int main(int argc, char* argv[]){
+	int average;
+	average = avg(4,2);
+	printf("Average of the two values is: %d.\n", average);
+	return 0;
+}
